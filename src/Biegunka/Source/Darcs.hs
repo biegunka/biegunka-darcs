d72d4881b3299a9c6089669b4bf77b009320a795
Matvey Aksenov <matvey.aksenov@gmail.com>
2013-02-03 18:43:57 +0400

    Revert failed attempt to monadize Script.

--- src/Biegunka/Source/Darcs.hs
+++ src/Biegunka/Source/Darcs.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE DataKinds     #-}
+{-# LANGUAGE DataKinds #-}
 {-# LANGUAGE UnicodeSyntax #-}
 -- | Biegunka.Source.Darcs - functions to work with darcs repositories as sources
 module Biegunka.Source.Darcs
@@ -6,24 +6,22 @@ module Biegunka.Source.Darcs
     darcs, darcs_
   ) where
 
-import           Control.Exception     (SomeException (..), fromException,
-                                        handle)
-import           Control.Monad.Free    (liftF)
-import           Data.Text             (pack)
-import           System.Directory      (doesDirectoryExist)
-import           System.Exit           (ExitCode (..))
-import           System.FilePath.Posix (takeDirectory)
+import Control.Exception (SomeException (..), fromException, handle)
+import System.Exit (ExitCode (..))
+import Control.Monad.Free    (liftF)
+import Data.Text             (pack)
+import System.Directory      (doesDirectoryExist)
+import System.FilePath.Posix (takeDirectory)
 
-import           Darcs.Commands        (DarcsCommand (commandCommand))
-import           Darcs.Commands.Get    (get)
-import           Darcs.Commands.Pull   (pull)
-import qualified Darcs.Flags           as F
-import           Darcs.Utils           (withCurrentDirectory)
+import           Darcs.Commands (DarcsCommand (commandCommand))
+import           Darcs.Commands.Get (get)
+import           Darcs.Commands.Pull (pull)
+import qualified Darcs.Flags as F
+import           Darcs.Utils (withCurrentDirectory)
 
 
-import           Biegunka.Language     (Command (S), Layer (Files, Source),
-                                        Script)
-import           Biegunka.Source.Dummy (sourceFailure)
+import Biegunka.Language (Command (S), Layer (Files, Source), Script)
+import Biegunka.Source.Dummy (sourceFailure)
 
 
 -- | Clone repository from the given url to specified path and/or pull.
@@ -38,7 +36,7 @@ import           Biegunka.Source.Dummy (sourceFailure)
 -- * link ${HOME}\/darcs\/repository to ${HOME}\/some\/not\/so\/long\/path
 --
 -- * link ${HOME}\/darcs\/repository\/important.file to ${HOME}\/.config
-darcs ∷ String → FilePath → Script Files () → Script Source ()
+darcs ∷ String → FilePath → Script Files → Script Source
 darcs url path script = liftF $ S url path script (updateDarcs url) ()
 
 
@@ -47,7 +45,7 @@ darcs url path script = liftF $ S url path script (updateDarcs url) ()
 -- > darcs_ "https://example.com/repository" "darcs/repository"
 --
 -- * get repository from https:\/\/example.com\/repository to ${HOME}\/darcs\/repository
-darcs_ ∷ String → FilePath → Script Source ()
+darcs_ ∷ String → FilePath → Script Source
 darcs_ url path = darcs url path (return ())
 
 
